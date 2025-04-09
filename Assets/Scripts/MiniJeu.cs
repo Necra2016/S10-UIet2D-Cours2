e07e487cacfa716612a951fb7246d8e04c9835b0
Necra2016 <jeremy18cote@gmail.com>
2025-04-09 15:49:45 -0400

    commits

--- Assets/Scripts/MiniJeu.cs
+++ Assets/Scripts/MiniJeu.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MiniJeu : MonoBehaviour
 {
     public float pointageTemps;
 
+    [SerializeField] TextMeshProUGUI _pts;
+
     void Start()
     {
         pointageTemps = 0;
+        InvokeRepeating("augPts", 0, 1);
+    }
+
+    void augPts()
+    {
+        _pts.text = "Points: " + pointageTemps.ToString("00");
     }
 }
